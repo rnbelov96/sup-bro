92cd84e0e76766c590f7e938c1b10d073d31ebe6
Belov Roman <rnbelov96@gmail.com>
2022-04-29 20:43:09 +0300

    Finish main part

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import './scss/style.scss';
 import './ts/dropdown';
-// import './ts/modals';
+import './ts/modals';
 // import './ts/slider';
 // import './ts/calc';
-// import './ts/setup-forms';
-// import './ts/f5leads-init';
-// import './ts/f5leads';
+import './ts/setup-forms';
+import './ts/f5leads-init';
+import './ts/f5leads';
